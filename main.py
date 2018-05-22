f620c20217b1dc325628207dbb78f31306f56671
UgainJain <UgainJain@gmail.com>
2018-05-22 22:42:15 +0530

    adding some changes according to the objectives...........

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ Consumer_secret = ""
 
 ### Token ###
 Token_Key = ""
-
 ### Token KEY ###
 Token_secret = ""
 
@@ -23,8 +22,8 @@ Token_secret = ""
 def retrieve_tweets() :   ####To retrieve the required tweets
     query = input("Enter the keyowords without hashtag :") ###ask for the query to search on twitter
     query = "#"+ query      ####Adding a "#" for better search results
-    Tweets = api.search(q=query,  lang="en", count = 200) ### Searching the twitter
-    return Tweets
+    Tweets = api.search(q=query,  lang="en",count =200) ### Searching the twitter
+    return  Tweets
 
 
 def print_tweets(tweets):   #### For printing the tweets
@@ -37,6 +36,13 @@ def print_tweets(tweets):   #### For printing the tweets
         print("---------------------------------------------")
 
 
+def extract_for_user():        ###Extracts tweets for perticular user
+    query = input("Enter the onscreen name of the user with an '@' in the front:")
+    Tweets = api.user_timeline(screen_name=query, count=200)    ###Search for tweets by the given screen name
+    print_tweets(Tweets)
+
+
+
 def no_of_followers(Tweet):
     print("Printing no of followers :-\nUSER        No of Followers")
     total = 0
@@ -46,17 +52,11 @@ def no_of_followers(Tweet):
     print("Total no of persons effected by the Tweets:" + str(total))
 
 
-def extract_for_user():        ###Extracts tweets for perticular user
-    query = input("Enter the onscreen name of the user with an '@' in the front:")
-    Tweets = api.user_timeline(screen_name=query, count=200)    ###Search for tweets by the given screen name
-    print_tweets(Tweets)
-
-
 def search_for_keywords():      ### search the tweets  for perticular keyword in the tweets by a perticular user
     user = input("Enter the name of the user:")
     query = []      ### Stores the keywords to be searched for
     tweets= []  ### to store the tweets
-    tweets2 = []        ### to store modified tweets
+    tweets2 = []        ### to storre modified tweetss
     shows= True
     while shows :       ### to store the no of keywords till we want
         qw = input("Enter  the keywords ro be searched for :")
@@ -71,7 +71,7 @@ def search_for_keywords():      ### search the tweets  for perticular keyword in
             print("invalid input  \n"
                   "Taking default input as 'No' ")
             shows = False
-    result = api.user_timeline(screen_name=user,count = 200) ### searches for the 200 tweets by the perticular user as the limit in tweepy is 200 only
+    result = api.user_timeline(screen_name=user,count = 200) ### searches for the 200 tweets by the perticular user
     for data in result:
         tweets.append(data)
     oldest = tweets[-1].id      ### stores the id of the last tweet retrieved
@@ -162,6 +162,134 @@ def tweet_on_Twitter():
     text = input("What do you want to tweet")
     api.update_status(text)
 
+
+def for_narendra_modi():
+    print("Retreving tweets")
+    Tweets = api.user_timeline(screen_name= "@narendramodi", count=200)  ###Search for tweets by the given screen name
+    print_tweets(Tweets)
+    query = []  ### Stores the keywords to be searched for
+    tweets = []  ### to store the tweets
+    tweets2 = []  ### to store modified tweets
+    shows = True
+    while shows:  ### to store the no of keywords till we want
+        qw = input("Enter  the keywords ro be searched for :")
+        query.append(qw)
+        ask = input("Do you want to add more keywords?(Y/N)")
+        ask = ask.upper()
+        if ask == "Y":
+            pass
+        elif ask == "N":
+            shows = False
+        else:
+            print("invalid input  \n"
+                  "Taking default input as 'No' ")
+            shows = False
+
+    for data in Tweets:
+        tweets.append(data)
+    oldest = tweets[-1].id  ### stores the id of the last tweet retrieved
+    oldest_at = tweets[-1].created_at  ### Stores the time and date of the last tweet retrieved
+    qu = True
+    while qu:  ###to retrieve more older tweets
+        qu2 = input("Do you want  more  tweets older than " + str(oldest_at) + "(Y/N)  :")
+        qu2 = qu2.upper()
+        if qu2 == "Y":
+            result = api.user_timeline(screen_name="@narendramodi", count=200,
+                                        max_id=oldest)  ###retrieve the tweet older then the given tweet id
+            for data in result:
+                tweets.append(data)  ###  appended the new tweets retrived
+            oldest = tweets[-1].id
+            oldest_at = tweets[-1].created_at
+        elif qu2 == "N":
+            qu = False
+        else:
+            print("invalid input  \n"
+                  "Taking default input as 'No' ")
+            qu = False
+    count = -1
+    for tweet1 in tweets:
+        count = count + 1  ###starts the list from beginning
+        tweet = tweet1.text
+        tweet = re.sub(r"http\S+", "", tweet)  ##### removes the URL from the text of tweet
+        tweets[count].text = tweet
+    for keyword in query:
+        for data in tweets:
+            tex = TextBlob(data.text)
+            qw = tex.find(keyword)
+            if qw != -1:
+                tweets2.append(data)  ### Appends the modified tweets
+            else:
+                pass
+    if len(tweets2) > 0:
+        print_tweets(tweets2)
+    else:
+        print("No tweet found with the related keywords")
+
+
+def for_donald_trump():
+    print("Retreving tweets")
+    Tweets = api.user_timeline(screen_name="@realdonaldtrump", count=200)  ###Search for tweets by the given screen name
+    print_tweets(Tweets)
+    query = []  ### Stores the keywords to be searched for
+    tweets = []  ### to store the tweets
+    tweets2 = []  ### to store modified tweets
+    shows = True
+    while shows:  ### to store the no of keywords till we want
+        qw = input("Enter  the keywords ro be searched for :")
+        query.append(qw)
+        ask = input("Do you want to add more keywords?(Y/N)")
+        ask = ask.upper()
+        if ask == "Y":
+            pass
+        elif ask == "N":
+            shows = False
+        else:
+            print("invalid input  \n"
+                  "Taking default input as 'No' ")
+            shows = False
+
+    for data in Tweets:
+        tweets.append(data)
+    oldest = tweets[-1].id  ### stores the id of the last tweet retrieved
+    oldest_at = tweets[-1].created_at  ### Stores the time and date of the last tweet retrieved
+    qu = True
+    while qu:  ###to retrieve more older tweets
+        qu2 = input("Do you want  more  tweets older than " + str(oldest_at) + "(Y/N)  :")
+        qu2 = qu2.upper()
+        if qu2 == "Y":
+            result = api.user_timeline(screen_name="@realdonaldtrump", count=200,
+                                        max_id=oldest)  ###retrieve the tweet older then the given tweet id
+            for data in result:
+                tweets.append(data)  ###  appended the new tweets retrived
+            oldest = tweets[-1].id
+            oldest_at = tweets[-1].created_at
+        elif qu2 == "N":
+            qu = False
+        else:
+            print("invalid input  \n"
+                  "Taking default input as 'No' ")
+            qu = False
+    count = -1
+    for tweet1 in tweets:
+        count = count + 1  ###starts the list from beginning
+        tweet = tweet1.text
+        tweet = re.sub(r"http\S+", "", tweet)  ##### removes the URL from the text of tweet
+        tweets[count].text = tweet
+    for keyword in query:
+        for data in tweets:
+            tex = TextBlob(data.text)
+            qw = tex.find(keyword)
+            if qw != -1:
+                tweets2.append(data)  ### Appends the modified tweets
+            else:
+                pass
+    if len(tweets2) > 0:
+        print_tweets(tweets2)
+    else:
+        print("No tweet found with the related keywords")
+
+
+
 auth = OAuthHandler(Consumer_key, Consumer_secret)      ###authorises the twitter handle
 auth.set_access_token(Token_Key, Token_secret)          ###Passes the access tokens and the key
 
@@ -177,7 +305,9 @@ while  menu_display :
                            '5.Send a direct message to someone \n'
                            '6.Remove stopWords from retrieved tweets \n'
                            '7.Tweet a message\n'
-                           '8.Exit \n'
+                           '8.Extract Tweets for donald trump\n'
+                           '9.Extract Tweets for narendra modi \n'
+                           '10.Exit \n'
                            'Enter your choice :\t '))
 
     if menuchoice == 1:
@@ -196,7 +326,11 @@ while  menu_display :
         remove_stopwords()
     elif menuchoice == 7:
         tweet_on_Twitter()
-    elif menuchoice == 8 :
+    elif menuchoice == 8:
+        for_donald_trump()
+    elif menuchoice == 9:
+        for_narendra_modi()
+    elif menuchoice == 10:
         menu_display = False
         exit()
     else:
